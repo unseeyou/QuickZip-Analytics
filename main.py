88c2ee156ab35e98969418adcd7b157b3829c8ad
unseeyou <unseeyoubestmod@gmail.com>
2026-02-26 14:40:56 +1100

    feat: basic PWA functionality

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from constants import app
+from flask import send_file, render_template
 
 
 @app.route("/")
 def index():
-    return "under construction"
+    return render_template("index.html")
+
+
+@app.route("/manifest.json")
+def serve_manifest():
+    return send_file("manifest.json", mimetype="application/manifest+json")
+
+
+@app.route("/sw.js")
+def serve_sw():
+    return send_file("static/js/sw.js", mimetype="application/javascript")
 
 
 if __name__ == "__main__":
